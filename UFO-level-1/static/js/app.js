c6c13932b9b4c07f17069b0548a23864b056bfe4
unknown <elliottmcf@gmail.com>
2020-12-17 17:03:57 -0500

    Converts landing page table loop into a function for implementation with filters

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,14 +1,20 @@
 // from data.js
 var tableData = data;
 
+var button = d3.select("#filter-btn");
+var form = d3.select("form");
+
 var tbody = d3.select("#table-area").select("tbody");
 
-// YOUR CODE HERE!
-data.forEach(sighting => {
-    var row = tbody.append("tr");
-    Object.entries(sighting).forEach(([key, value]) => {
-        var cell = row.append("td");
-        cell.text(value);
-    });
-});
+addTable(tableData);
 
+
+function addTable(sightings) {
+    sightings.forEach(sighting => {
+        var row = tbody.append("tr");
+        Object.entries(sighting).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+}
